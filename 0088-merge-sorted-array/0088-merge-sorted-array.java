ddf38cbadc2a116e5f93e5e0b3e49e9f5ca83b49
Hilbert Kong <kongliangxm@foxmail.com>
2024-05-11 18:27:29 +0900

    Time: 0 ms (100.00%), Space: 42 MB (52.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,23 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] temp = new int[m];
-        for (int i = 0; i < m; i++) {
-            temp[i] = nums1[i];
-        }
+    public void merge(int[] nums1, int m, int[] nums2, int n) {       
+        int idx1 = m - 1;
+        int idx2 = n - 1;
         
-        int idx1 = 0;
-        int idx2 = 0;
-        
-        for (int i = 0; i < m + n; i++) {
-            if (idx1 == m && idx2 == n) {
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (idx1 == -1 && idx2 == -1) {
                 break;
-            } else if (idx1 == m) {
+            } else if (idx1 == -1) {
                 nums1[i] = nums2[idx2];
-                idx2++;
-            } else if (idx2 == n) {
-                nums1[i] = temp[idx1];
-                idx1++;
-            } else if (temp[idx1] < nums2[idx2]) {
-                nums1[i] = temp[idx1];
-                idx1++;
+                idx2--;
+            } else if (idx2 == -1) {
+                nums1[i] = nums1[idx1];
+                idx1--;
+            } else if (nums1[idx1] < nums2[idx2]) {
+                nums1[i] = nums2[idx2];
+                idx2--;
             } else {
-                nums1[i] = nums2[idx2];
-                idx2++;
+                nums1[i] = nums1[idx1];
+                idx1--;
             }
         }
     }
