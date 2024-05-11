a76be176d361e4acd8b49937f4f71e603910fd0d
Hilbert Kong <kongliangxm@foxmail.com>
2024-05-11 19:08:44 +0900

    Time: 1 ms (100.00%), Space: 45.7 MB (78.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,9 +2,13 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int i = 0;
         for (int j = 0; j < nums.length; j++) {
-            if (nums[j] == 0) continue;
-            nums[i] = nums[j];
-            if (i != j) nums[j] = 0;
+            if (nums[j] != 0) {
+                nums[i]= nums[j];
+                i++;
+            }
+        }
+        while (i < nums.length) {
+            nums[i] = 0;
             i++;
         }
     }
