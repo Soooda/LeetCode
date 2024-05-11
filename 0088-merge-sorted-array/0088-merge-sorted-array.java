399d44ac30d8d99eacbf771bc59670dd6f9911df
Hilbert Kong <kongliangxm@foxmail.com>
2024-05-11 18:36:05 +0900

    Time: 0 ms (100.00%), Space: 41.8 MB (82.16%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,20 +4,12 @@ class Solution {
         int idx2 = n - 1;
         
         for (int i = m + n - 1; i >= 0; i--) {
-            if (idx1 == -1 && idx2 == -1) {
-                break;
-            } else if (idx1 == -1) {
-                nums1[i] = nums2[idx2];
-                idx2--;
-            } else if (idx2 == -1) {
+            if (idx2 < 0 || (idx1 >= 0 && nums1[idx1] > nums2[idx2])) {
                 nums1[i] = nums1[idx1];
                 idx1--;
-            } else if (nums1[idx1] < nums2[idx2]) {
-                nums1[i] = nums2[idx2];
-                idx2--;
             } else {
-                nums1[i] = nums1[idx1];
-                idx1--;
+                nums1[i] = nums2[idx2];
+                idx2--;
             }
         }
     }
